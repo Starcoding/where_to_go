5a9dcfa0be9fc5b60ec954f7d892afce2ddce34f
Starcoding <fagost32@gmail.com>
2020-10-18 12:02:21 +0300

    Fixed DEBUG setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+if os.getenv('DEBUG') == "True":
+	DEBUG = True
+else:
+	DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com", 'localhost']
 
