b7203ea1be244c361b537eb0cb0e2d0b1493f387
Starcoding <fagost32@gmail.com>
2021-08-03 12:50:31 +0300

    Added verbose names for models

--- places/models.py
+++ places/models.py
@@ -3,23 +3,26 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    description_short = HTMLField(blank=True)
-    description_long = HTMLField(blank=True)
-    latitude = models.FloatField(max_length=25, blank=True)
-    longitude = models.FloatField(max_length=25, blank=True)
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description_short = HTMLField(blank=True, verbose_name='Короткое описание')
+    description_long = HTMLField(blank=True, verbose_name='Описание')
+    latitude = models.FloatField(max_length=25, blank=True, verbose_name='Широта')
+    longitude = models.FloatField(max_length=25, blank=True, verbose_name='Долгота')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Места'
 
 class Image(models.Model):
-    image = models.ImageField()
-    number = models.PositiveIntegerField(default=0, blank=True)
-    place = models.ForeignKey(Place, related_name="images", on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(verbose_name='Изображение')
+    number = models.PositiveIntegerField(default=0, blank=True, verbose_name='Порядковый номер')
+    place = models.ForeignKey(Place, related_name="images", on_delete=models.CASCADE, blank=True, verbose_name='Место')
 
     def __str__(self):
         return f'{self.number}, {self.place.title}'
 
     class Meta:
         ordering = ('number',)
+        verbose_name = 'Изображения'
