6325df9366aaae756e5784db4bb3e672e53e81b9
Starcoding <fagost32@gmail.com>
2021-08-03 13:55:22 +0300

    Removed select field from admin page for images

--- places/admin.py
+++ places/admin.py
@@ -11,14 +11,19 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     readonly_fields = ("image_preview",)
 
+
     def image_preview(self, obj):
         url = obj.image.url
         return format_html('<img src="{}" height="100" width="200" />', url)
 
+class ImageAdmin(admin.ModelAdmin):
+    raw_id_fields = ("place",)
+    autocomplete_fields = ['place']
+
 
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
+    search_fields = ['title']
 
-
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Place, PlaceAdmin)
