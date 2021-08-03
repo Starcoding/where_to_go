7a03572b9cc521911fc0f5619023578e40e15599
Starcoding <fagost32@gmail.com>
2021-08-03 11:33:13 +0300

    Remover unnecessary requirements and added enumerating instead of counting

--- places/management/commands/loadplace.py
+++ places/management/commands/loadplace.py
@@ -1,9 +1,6 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from places.models import Place, Image
 import requests
-import json
-from PIL import Image as PIL_Image
-from io import BytesIO
 from django.core.files.base import ContentFile
 
 class Command(BaseCommand):
@@ -15,7 +12,7 @@ class Command(BaseCommand):
     def handle(selsf, *args, **options):
         for url in options['urls']:
             # try:                
-            place_raw = requests.get(url).json()
+            place_raw = requests.get(url).json()    
             place, created = Place.objects.get_or_create(
                 title = place_raw["title"],
                 description_short = place_raw["description_short"],
@@ -23,12 +20,10 @@ class Command(BaseCommand):
                 latitude = place_raw["coordinates"]["lat"],
                 longitude = place_raw["coordinates"]["lng"],
             )
-            img_number = 1
-            for img_url in place_raw["imgs"]:
+            for img_number, img_url in enumerate(place_raw["imgs"], 1):
                 request = requests.get(img_url)
                 img_name = img_url.split("/")[len(img_url.split("/"))-1]
                 img = Image.objects.get_or_create(places=place, number=img_number)
-                img_number = img_number + 1
                 f = ContentFile(request.content)
                 img[0].image.save(img_name, f, save=True) 
 
