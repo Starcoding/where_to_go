b81795ef704e4db98238940a23c6024793c5fb06
Starcoding <fagost32@gmail.com>
2021-08-04 20:42:29 +0300

    Improved loadplace command for quicker finding in DB

--- places/management/commands/loadplace.py
+++ places/management/commands/loadplace.py
@@ -14,12 +14,14 @@ class Command(BaseCommand):
     def handle(selsf, *args, **options):
         for url in options['urls']:    
             place_raw = requests.get(url).json()    
-            place, created = Place.objects.get_or_create(
+            place, created = Place.objects.update_or_create(
                 title = place_raw["title"],
-                description_short = place_raw["description_short"],
-                description_long = place_raw["description_long"],
-                latitude = place_raw["coordinates"]["lat"],
-                longitude = place_raw["coordinates"]["lng"],
+                defaults={
+                    'description_short': place_raw["description_short"],
+                    'description_long': place_raw["description_long"],
+                    'latitude': place_raw["coordinates"]["lat"],
+                    'longitude': place_raw["coordinates"]["lng"],
+                }
             )
             for img_number, img_url in enumerate(place_raw["imgs"], 1):
                 request = requests.get(img_url)
